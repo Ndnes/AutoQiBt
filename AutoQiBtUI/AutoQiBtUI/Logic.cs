08b0c88ae04ed209c4dbdcab41d4d4d2e121fc30
Terje Molnes <tmolnez@gmail.com>
2019-06-30 08:20:00 +0200

    Add logic class

---
+++ AutoQiBtUI/AutoQiBtUI/Logic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace AutoQiBtUI
+{
+    public class Logic
+    {
+        /// <summary>
+        /// The previous status of bluetooth activation.
+        /// True: Bluetooth was active before Qi charging.
+        /// </summary>
+        private bool _previousStatus;
+
+        public bool PreviousStatus
+        {
+            get { return _previousStatus; }
+            set { _previousStatus = value; }
+        }
+
+
+
+        void onQiChargeDetectedOn()
+        {
+            PreviousStatus = checkBtStatus();
+        }
+
+        void onQiChargeDetectedOff()
+        {
+          
+        }
+
+
+        bool checkIfChargingWireless()
+        {
+            var state = Battery.State;
+
+            if (state == BatteryState.Charging)
+            {
+                var source = Battery.PowerSource;
+                if (source == BatteryPowerSource.Wireless)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool checkBtStatus()
+        {
+            return false;
+        }
+
+        void activateBt()
+        {
+
+        }
+        void deactivateBt()
+        {
+
+        }
+    }
+}
