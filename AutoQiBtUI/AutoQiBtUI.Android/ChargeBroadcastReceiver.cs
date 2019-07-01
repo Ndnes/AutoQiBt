bb41462690d714c27517448c6ba4731a726b037f
Terje Molnes <tmolnez@gmail.com>
2019-07-01 08:19:05 +0200

    Add broadcast receiver class for Intent.ActionBatteryChanged

---
+++ AutoQiBtUI/AutoQiBtUI.Android/ChargeBroadcastReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AutoQiBtUI.Droid
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { Intent.ActionBatteryChanged })]
+    class ChargeBroadcastReceiver : BroadcastReceiver
+    {
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            ToneGenerator t = new ToneGenerator(Android.Media.Stream.Alarm, 100);
+            Tone tTone = Tone.SupPip;
+            t.StartTone(tTone, 1000);
+        }
+    }
+}
--- AutoQiBtUI/AutoQiBtUI.Android/MainActivity.cs
+++ AutoQiBtUI/AutoQiBtUI.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Android.Media;
+using Android.Content;
 
 namespace AutoQiBtUI.Droid
 {
@@ -21,6 +24,7 @@ namespace AutoQiBtUI.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FormsMaterial.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
